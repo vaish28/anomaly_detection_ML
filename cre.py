3edb5eff4594b4560545d25ad5744b75f617a236
vaish28 <43900410+vaish28@users.noreply.github.com>
2020-04-20 22:05:35 +0530

    Add files via upload

---
+++ cre.py
@@ -0,0 +1,8 @@
+SSID='twilio account ssid'
+AUTH_TOKEN='auth token of twilio'
+FROM_NUMBER='auto-generated number of twilio'
+TO_NUMBER='number you want to send sms to'
+API_KEY='api key of bolt iot cloud'
+DEVICE_ID='device id of bolt cloud'
+FRAME_SIZE= 10
+MUL_FACTOR= 6
